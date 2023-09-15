61619b47894421a6e4aef8b4a6d1aee0953aad5d
Vladimir <vovka296@yandex.ru>
2023-09-15 19:33:01 +0500

    add l7

--- Lesson6/n3.py
+++ Lesson6/n3.py
@@ -1,11 +1,11 @@
 import random
-#from modules import validate_queens
-def validate_queens(queens):
-    for i in range (len(queens)):
-        for j in range(i+1, len(queens)):
-            if queens[i][0] == queens[j][0] or queens[i][1] == queens[j][1] or abs(queens[i][0]-queens[j][0]) == abs(queens[i][1] - queens[j][1]):
-                return False
-    return True
+from modules import validate_queens
+# def validate_queens(queens):
+#     for i in range (len(queens)):
+#         for j in range(i+1, len(queens)):
+#             if queens[i][0] == queens[j][0] or queens[i][1] == queens[j][1] or abs(queens[i][0]-queens[j][0]) == abs(queens[i][1] - queens[j][1]):
+#                 return False
+#     return True
 
 def generate_positions():
     for i in range(4):
