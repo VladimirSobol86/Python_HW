8fdc306c9e161d264dce92b90b7023b4059b8e0c
Vladimir <vovka296@yandex.ru>
2023-09-28 23:14:52 +0500

    d

--- Lesson11/seminar.py
+++ Lesson11/seminar.py
@@ -3,7 +3,7 @@
 # будут доступны все возможности str
 # дополнительно хранятся имя автора строки и время создания# (time.time)
 
-# from time import time
+from time import time
 
 class MyString(str):
     """It is help for MyString"""
